c21757774d5f61d20be47538d3179af4d84268e1
Alon Maltsev <alonmaltsev@gmail.com>
2025-01-24 19:31:22 +0200

    new spells downloaded

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import BDDataUploader from "../BDDataUploader/BDDataUploader";
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <BDDataUploader/>
       <p>© 2025 DND Multiverse. Все права защищены.</p>
     </footer>
   );
